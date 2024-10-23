da68cadba70be90b8e47c35805ab76c82fd052b5
Marius <40984378+MariusBrummer@users.noreply.github.com>
2024-10-23 18:08:25 +0200

    Fixed reporting and video recording

--- cypress.config.js
+++ cypress.config.js
@@ -6,18 +6,21 @@ module.exports = defineConfig({
     supportFile: false,
     watchForFileChanges: false,
     defaultCommandTimeout: 60000,
-    reporter: 'mochawesome',
+    video: true,
+    screenshotsFolder: 'cypress/screenshots',
+    videosFolder: 'cypress/videos',
+    reporter: 'cypress-mochawesome-reporter', // Ensure correct reporter name
     reporterOptions: {
-      reportDir: 'cypress/reports', 
-      overwrite: false, 
-      html: true, 
-      json: true, 
-      reportFilename: 'report.html', 
-      embeddedScreenshots: true, 
-      inlineAssets: true,
+      reportDir: 'cypress/reports/mocha', // Set reports directory
+      overwrite: false, // Do not overwrite existing reports
+      html: true, // Generate HTML report
+      json: true, // Generate JSON report
+      reportFilename: 'report', // Name of the report file
+      embeddedScreenshots: true, // Embed screenshots
+      inlineAssets: true, // Inline assets
     },
   },
   setupNodeEvents(on, config) {
-     },
+    require('cypress-mochawesome-reporter/plugin')(on);
+  },
 });
-
